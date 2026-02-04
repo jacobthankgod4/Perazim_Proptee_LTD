681e5138984ed633ee68395700ea1963a912e7b8
jacobthankgod4 <jacobthankgod4@gmail.com>
2026-02-04 14:55:46 +0100

    Add error handling

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-error_reporting(E_ALL); // Report all types of errors
-ini_set('display_errors', 1); // Display errors on the screen (for development)
-ini_set('display_startup_errors', 1); // Display errors that occur during PHP's startup
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 $page_title = 'Home Page ';
 
-include 'Administration/config.inc.php';
-
-include "Administration/mysql.inc.php";
-
-include "includes/view/header.php";
-
-include "includes/view/processor/home.data.config.php";
-
-include "includes/view/home.php";
-
-include "includes/view/footer.php";
+try {
+    include 'Administration/config.inc.php';
+    include "Administration/mysql.inc.php";
+    include "includes/view/header.php";
+    include "includes/view/processor/home.data.config.php";
+    include "includes/view/home.php";
+    include "includes/view/footer.php";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+    error_log($e->getMessage());
+}
